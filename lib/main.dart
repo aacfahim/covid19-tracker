18bbe910cb62c37da2cf372ef443cc166f2a43cd
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-03-25 11:30:28 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:covid19_tracker/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID 19 Tracker',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
