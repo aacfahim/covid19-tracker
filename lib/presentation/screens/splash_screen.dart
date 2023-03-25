afd0a24a81e43412e80dde10481f1b454339357f
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-03-25 12:38:49 +0600

    splash screen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,14 +1,61 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:covid19_tracker/presentation/screens/global_stat_screen.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(vsync: this, duration: Duration(seconds: 3))
+        ..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    Timer(
+        Duration(seconds: 2),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => GlobalStatScreen())));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("data")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              child: SizedBox(
+                  child: Image.asset(
+                'images/virus.png',
+                scale: 2,
+              )),
+              builder: (context, child) {
+                return Transform.rotate(
+                    angle: _controller.value * 2 * math.pi, child: child);
+              },
+            ),
+            const Text("COVID-19 Tracker Worldwide",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
     );
   }
 }
